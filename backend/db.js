59c1aa60e10ae52e5634178c4d314689ef300c1c
PriyanshiPatel360 <patelp79535@gmail.com>
2026-04-22 22:51:07 +0530

    updated

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Rrampatel@720',
+    password: 'password',
     database: 'music_library'
 });
 
